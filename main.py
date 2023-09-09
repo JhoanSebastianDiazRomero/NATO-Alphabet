201d4f2e528f4e5e2c2ead0e20cf5b78c8c6d7eb
Jhoan Sebastian Díaz Romero <js.diazr@uniandes.edu.co>
2023-09-08 21:26:03 -0500

    Added error handling

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pandas
 
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(nato_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Input a word to get NATO version: ").upper()
-nato_input = [nato_dict[letter] for letter in user_input]
-print(nato_input)
+
+def generate_phonetic():
+    try:
+        user_input = input("Input a word to get NATO version: ").upper()
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        nato_input = [nato_dict[letter] for letter in user_input]
+        print(nato_input)
+
+generate_phonetic()
